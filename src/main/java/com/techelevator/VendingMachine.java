23f07f0a15f9fcc535455fba6ed1713aa14588f2
Andrew Le <andrew.le.careers@gmail.com>
2024-06-06 18:57:11 -0500

    Added the printTransaction method to print one type of interaction to be later saved in log.txt

--- src/main/java/com/techelevator/VendingMachine.java
+++ src/main/java/com/techelevator/VendingMachine.java
@@ -7,13 +7,17 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.math.BigDecimal;
+import java.text.NumberFormat;
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 public final class VendingMachine {
     public VendingMachine(int productsPerSlot, String inventoryFilepath) throws InventoryLoadException {
         this.PRODUCTS_PER_SLOT = productsPerSlot;
 
+        currentBalance = BigDecimal.ZERO;
         inventoryList = new ArrayList<>();
         inventoryMap = loadInventory(readInventoryFile(inventoryFilepath));
     }
@@ -28,7 +32,7 @@ public final class VendingMachine {
         if (funds.compareTo(BigDecimal.ZERO) == -1) return;
 
         currentBalance = currentBalance.add(funds);
-        //logTransaction(ADD_FUNDS_LOG_TEXT, funds, currentBalance);
+        printTransaction(ADD_FUNDS_LOG_TEXT, funds, currentBalance);
     }
 
     public void selectProduct(String slotNumber) {
@@ -45,13 +49,35 @@ public final class VendingMachine {
 
         product.dispense();
         currentBalance = currentBalance.subtract(product.getPrice());
-        //logTransaction(String.format("%s %s", product.getDescription(), slotNumber), product.getPrice(), currentBalance);
+        printTransaction(String.format("%s %s", product.getDescription(), slotNumber), product.getPrice(), currentBalance);
     }
+
+
 
     public void finishTransaction() {
 
 
         //logTransaction()
+    }
+
+    private static void printTransaction(String transactionName, BigDecimal cost, BigDecimal balance){
+        //Date
+        LocalDate dateOfOperation = LocalDate.now();
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        String date = dateFormat.format(dateOfOperation);
+
+        //Time
+        LocalTime timeOfOperation = LocalTime.now();
+        DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("hh:mm:ss a");
+        String time = timeFormat.format(timeOfOperation);
+
+        //Prepare transaction message
+        String transaction = String.format("%s  %s  %s", date,time,transactionName);
+        for (int i = 0; i < 14-transactionName.length(); i++) {
+            transaction = transaction.concat(" ");
+        }
+        transaction = transaction.concat(String.format("| %s  %s",currencyFormat.format(cost.doubleValue()), currencyFormat.format(balance.doubleValue())));
+        System.out.println(transaction);
     }
 
     private HashMap<String, Dispenser> loadInventory(List<String[]> tokenLines) throws InventoryLoadException {
@@ -129,4 +155,5 @@ public final class VendingMachine {
     private final int PRODUCTS_PER_SLOT;
     private static final String ADD_FUNDS_LOG_TEXT = "FEED MONEY";
     private static final String CHANGE_LOG_TEXT = "GIVE CHANGE";
+    private final static NumberFormat currencyFormat = NumberFormat.getCurrencyInstance(Locale.US);
 }
