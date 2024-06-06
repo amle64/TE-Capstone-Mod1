bfb3682b3b74491e1829c6e2f359feeb116a3220
MaikoDev <phyonaung@ymail.com>
2024-06-05 23:40:24 -0700

    Switched out String based menu's for Enum based menu

---
+++ src/main/java/com/techelevator/view/MainMenu.java
@@ -0,0 +1,23 @@
+package com.techelevator.view;
+
+public class MainMenu {
+    public enum Options implements IMenuOptions {
+        DisplayItems(0),
+        Purchase(1),
+        Exit(2),
+        Secret(3);
+
+        public final int value;
+
+        @Override
+        public String getOptionText() {
+            return OPTIONS_TEXT[value];
+        }
+
+        private Options(int value) {
+            this.value = value;
+        }
+    }
+
+    private static final String[] OPTIONS_TEXT = new String[]{ "Display Vending Machine Items", "Purchase", "Exit", "*Sales Report" };
+}
---
+++ src/main/java/com/techelevator/view/PurchaseMenu.java
@@ -0,0 +1,22 @@
+package com.techelevator.view;
+
+public class PurchaseMenu {
+    public enum Options implements IMenuOptions {
+        FeedMoney(0),
+        SelectProduct(1),
+        FinishTransaction(2);
+
+        public final int value;
+
+        @Override
+        public String getOptionText() {
+            return OPTIONS_TEXT[value];
+        }
+
+        private Options(int value) {
+            this.value = value;
+        }
+    }
+
+    private static final String[] OPTIONS_TEXT = new String[]{ "Feed Money", "Select Product", "Finish Transaction" };
+}
