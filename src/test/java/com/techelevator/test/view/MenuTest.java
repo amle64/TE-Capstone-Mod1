c8d0a76b68fae6d2a19dac4dad3f7b8416def105
Phyo Naung <phyonaung32@yahoo.com>
2024-06-06 22:08:58 -0700

    Moved test to com.techelevator.test

--- src/test/java/com/techelevator/test/view/MenuTest.java
+++ src/test/java/com/techelevator/test/view/MenuTest.java
@@ -21,30 +21,30 @@ public class MenuTest {
 
 	@Test
 	public void displays_a_list_of_menu_options_and_prompts_user_to_make_a_choice() {
-		Object[] options = new Object[] { Integer.valueOf(3), "Blind", "Mice" };
+		MainMenu.Options[] options = new MainMenu.Options[] { MainMenu.Options.DisplayItems, MainMenu.Options.Purchase };
 		VendingMenu menu = getMenuForTesting();
 
 		menu.getChoiceFromOptions(options);
 
-		String expected = System.lineSeparator() + "1) " + options[0].toString() + System.lineSeparator() + "2) " + options[1].toString() + System.lineSeparator() + "3) "
-				+ options[2].toString() + System.lineSeparator() + System.lineSeparator() + "Please choose an option >>> ";
+		String expected = System.lineSeparator() + "1) " + options[0].toString() + System.lineSeparator() + "2) " + options[1].toString() +
+				System.lineSeparator() + System.lineSeparator() + "Please choose an option >>> ";
 		Assert.assertEquals(expected, output.toString());
 	}
 
 	@Test
 	public void returns_object_corresponding_to_user_choice() {
-		Integer expected = Integer.valueOf(456);
-		Integer[] options = new Integer[] { Integer.valueOf(123), expected, Integer.valueOf(789) };
+		PurchaseMenu.Options expected = PurchaseMenu.Options.FinishTransaction;
+		PurchaseMenu.Options[] options = new PurchaseMenu.Options[] { PurchaseMenu.Options.FeedMoney, expected, PurchaseMenu.Options.FinishTransaction };
 		VendingMenu menu = getMenuForTestingWithUserInput("2" + System.lineSeparator());
 
-		Integer result = (Integer) menu.getChoiceFromOptions(options);
+		PurchaseMenu.Options result = (PurchaseMenu.Options) menu.getChoiceFromOptions(options);
 
 		Assert.assertEquals(expected, result);
 	}
 
 	@Test
 	public void redisplays_menu_if_user_does_not_choose_valid_option() {
-		Object[] options = new Object[] { "Larry", "Curly", "Moe" };
+		MainMenu.Options[] options = new MainMenu.Options[] { MainMenu.Options.DisplayItems, MainMenu.Options.Purchase, MainMenu.Options.Exit };
 		VendingMenu menu = getMenuForTestingWithUserInput("4" + System.lineSeparator() + "1" + System.lineSeparator());
 
 		menu.getChoiceFromOptions(options);
@@ -59,7 +59,7 @@ public class MenuTest {
 
 	@Test
 	public void redisplays_menu_if_user_chooses_option_less_than_1() {
-		Object[] options = new Object[] { "Larry", "Curly", "Moe" };
+		PurchaseMenu.Options[] options = new PurchaseMenu.Options[] { PurchaseMenu.Options.FinishTransaction, PurchaseMenu.Options.SelectProduct, PurchaseMenu.Options.FeedMoney };
 		VendingMenu menu = getMenuForTestingWithUserInput("0" + System.lineSeparator() + "1" + System.lineSeparator());
 
 		menu.getChoiceFromOptions(options);
@@ -74,7 +74,7 @@ public class MenuTest {
 
 	@Test
 	public void redisplays_menu_if_user_enters_garbage() {
-		Object[] options = new Object[] { "Larry", "Curly", "Moe" };
+		MainMenu.Options[] options = new MainMenu.Options[] { MainMenu.Options.Secret, MainMenu.Options.DisplayItems, MainMenu.Options.Exit };
 		VendingMenu menu = getMenuForTestingWithUserInput("Mickey Mouse" + System.lineSeparator() + "1" + System.lineSeparator());
 
 		menu.getChoiceFromOptions(options);
