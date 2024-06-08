d43090ee918865d436489076c4e4756847364414
Phyo Naung <phyonaung32@yahoo.com>
2024-06-07 23:19:52 -0700

    Fixed import issue with VendingMenu.java

--- src/main/java/com/techelevator/view/VendingMenu.java
+++ src/main/java/com/techelevator/view/VendingMenu.java
@@ -3,83 +3,81 @@ package com.techelevator.view;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.lang.reflect.Type;
 import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class VendingMenu {
+    public VendingMenu(InputStream input, OutputStream output) {
+        this.out = new PrintWriter(output);
+        this.in = new Scanner(input);
+    }
 
-	private final PrintWriter out;
-	private final Scanner in;
+    public IMenuOptions getChoiceFromOptions(IMenuOptions[] options) {
+        IMenuOptions choice = null;
+        while (choice == null) {
+            displayMenuOptions(options);
+            choice = getChoiceFromUserInput(options);
+        }
+        return choice;
+    }
 
-	public VendingMenu(InputStream input, OutputStream output) {
-		this.out = new PrintWriter(output);
-		this.in = new Scanner(input);
-	}
+    public String getStringInput() {
+        return in.nextLine();
+    }
 
-	public IMenuOptions getChoiceFromOptions(IMenuOptions[] options) {
-		IMenuOptions choice = null;
-		while (choice == null) {
-			displayMenuOptions(options);
-			choice = getChoiceFromUserInput(options);
-		}
-		return choice;
-	}
+    public int getIntInput() {
+        int input = 0;
 
-	public String getStringInput() {
-		return in.nextLine();
-	}
+        try {
+            input = Integer.parseInt(in.nextLine());
+        } catch (NumberFormatException ex) {
+            out.println("Input is not a number!");
+            out.flush();
+        }
 
-	public int getIntInput() {
-		int input = 0;
+        return input;
+    }
 
-		try {
-			input = Integer.parseInt(in.nextLine());
-		} catch (NumberFormatException ex) {
-			out.println("Input is not a number!");
-			out.flush();
-		}
+    public BigDecimal getMoneyInput() {
+        BigDecimal input = BigDecimal.ZERO;
 
-		return input;
-	}
+        try {
+            input = new BigDecimal(in.nextLine());
+        } catch(NumberFormatException ex) {
+            out.println("Input is not a number!");
+            out.flush();
+        }
 
-	public BigDecimal getMoneyInput() {
-		BigDecimal input = BigDecimal.ZERO;
+        return input;
+    }
 
-		try {
-			input = new BigDecimal(in.nextLine());
-		} catch(NumberFormatException ex) {
-			out.println("Input is not a number!");
-			out.flush();
-		}
+    private IMenuOptions getChoiceFromUserInput(IMenuOptions[] options) {
+        IMenuOptions choice = null;
+        String userInput = in.nextLine();
+        try {
+            int selectedOption = Integer.valueOf(userInput);
+            if (selectedOption > 0 && selectedOption <= options.length) {
+                choice = options[selectedOption - 1];
+            }
+        } catch (NumberFormatException e) {
+            // eat the exception, an error message will be displayed below since choice will be null
+        }
+        if (choice == null) {
+            out.println(System.lineSeparator() + "*** " + userInput + " is not a valid option ***" + System.lineSeparator());
+        }
+        return choice;
+    }
 
-		return input;
-	}
+    private void displayMenuOptions(IMenuOptions[] options) {
+        out.println();
+        for (int i = 0; i < options.length; i++) {
+            int optionNum = i + 1;
+            out.println(optionNum + ") " + options[i].getOptionText());
+        }
+        out.print(System.lineSeparator() + "Please choose an option >>> ");
+        out.flush();
+    }
 
-	private IMenuOptions getChoiceFromUserInput(IMenuOptions[] options) {
-		IMenuOptions choice = null;
-		String userInput = in.nextLine();
-		try {
-			int selectedOption = Integer.valueOf(userInput);
-			if (selectedOption > 0 && selectedOption <= options.length) {
-				choice = options[selectedOption - 1];
-			}
-		} catch (NumberFormatException e) {
-			// eat the exception, an error message will be displayed below since choice will be null
-		}
-		if (choice == null) {
-			out.println(System.lineSeparator() + "*** " + userInput + " is not a valid option ***" + System.lineSeparator());
-		}
-		return choice;
-	}
-
-	private void displayMenuOptions(IMenuOptions[] options) {
-		out.println();
-		for (int i = 0; i < options.length; i++) {
-			int optionNum = i + 1;
-			out.println(optionNum + ") " + options[i].getOptionText());
-		}
-		out.print(System.lineSeparator() + "Please choose an option >>> ");
-		out.flush();
-	}
+    private final PrintWriter out;
+    private final Scanner in;
 }
