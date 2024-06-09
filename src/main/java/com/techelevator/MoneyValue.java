c192ecfcee44c472c760ac1206ab3ace23edc0c7
Andrew Le <andrew.le.careers@gmail.com>
2024-06-09 16:10:03 -0500

    Created MoneyValue Class to be used in the Change Class

---
+++ src/main/java/com/techelevator/MoneyValue.java
@@ -0,0 +1,13 @@
+package com.techelevator;
+
+public class MoneyValue {
+    public int textIndex;
+
+    public int value;
+
+    public MoneyValue(int textIndex, int value){
+        this.textIndex = textIndex;
+        this.value = value;
+
+    }
+}
