c62b8f2b0d5c49b9779032569011b6b410f3429b
Phyo Naung <phyonaung32@yahoo.com>
2024-06-09 20:00:40 -0700

    Cleand up code and added code conventions

--- src/main/java/com/techelevator/MoneyValue.java
+++ src/main/java/com/techelevator/MoneyValue.java
@@ -1,13 +1,11 @@
 package com.techelevator;
 
 public class MoneyValue {
-    public int textIndex;
-
-    public int value;
-
     public MoneyValue(int textIndex, int value){
         this.textIndex = textIndex;
         this.value = value;
-
     }
+
+    public int textIndex;
+    public int value;
 }
